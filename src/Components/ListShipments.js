45e45bdd8e9353b183591a109d73859faeed1ef9
kirandeep123 <me@kiran.cc>
2019-08-08 14:32:53 +0530

    fetch call

--- src/Components/ListShipments.js
+++ src/Components/ListShipments.js
@@ -2,6 +2,10 @@ import React from 'react';
 import MaterialTable from 'material-table';
 import { getThemeProps } from '@material-ui/styles';
 import TablePagination from '@material-ui/core/TablePagination';
+import axios from 'axios';
+import { async } from 'q';
+import { func } from 'prop-types';
+
 
 export const ListShipments = (props)=>{
     const [state, setState] = React.useState({
@@ -18,24 +22,16 @@ export const ListShipments = (props)=>{
         data:props.shipments
         
       });
-
       return (
         <div style={{ maxWidth: "100%" }}>
 
         <MaterialTable
           title="Shipments Details"
           columns={state.columns}
-          data={query =>
-            new Promise((resolve, reject) => {
-                // prepare your data and then call resolve like this:
-                resolve({
-                    data: props.shipments, // your data array
-                    page : 1,// current page number
-                    totalCount:10 // total page number
-                });
-            })
-        }
+          data ={state.data}
+      
           editable={{
+
             onRowAdd: newData =>
               new Promise(resolve => {
                 setTimeout(() => {
@@ -46,12 +42,29 @@ export const ListShipments = (props)=>{
                 }, 600);
               }),
             onRowUpdate: (newData, oldData) =>
+           
               new Promise(resolve => {
                 setTimeout(() => {
                   resolve();
                   const data = [...state.data];
                   data[data.indexOf(oldData)] = newData;
                   setState({ ...state, data });
+                 //------------
+                 fetch('http://localhost:3000/shipments/321126', {
+                  method: 'put',
+                  
+                  body: JSON.stringify(newData),
+                
+                  headers: {
+                    'Accept': 'application/json'
+                  },
+                
+                  credentials: 'same-origin', // send cookies
+                  credentials: 'include',     // send cookies, even in CORS
+                
+                })
+                //---------
+    
                 }, 600);
               }),
             onRowDelete: oldData =>
@@ -65,7 +78,6 @@ export const ListShipments = (props)=>{
               }),
           }}
         />
-        <TablePagination/>
         </div>
       );
     
